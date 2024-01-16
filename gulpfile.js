524deb55459f0b04aa4b6b898b7b8e8f31fd6833
mdmunnaahmned <msdmunna77@gmail.com>
2024-01-16 11:31:54 +0600

    gulp-file,cleaning scss,  --upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,49 @@ const gulp = require("gulp");
 const { series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
-const cssbeautify = require("gulp-cssbeautify");
 const fileinclude = require("gulp-file-include");
-const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
 
 function style() {
-	return gulp.src("./src/assets/sass/main.scss").pipe(sourcemaps.init()).pipe(sass().on("error", sass.logError)).pipe(sass()).pipe(sourcemaps.write(".")).pipe(cssbeautify()).pipe(gulp.dest("./src/assets/css")).pipe(browserSync.stream());
+    return gulp
+        .src("./src/assets/sass/main.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("./src/assets/css"))
+        .pipe(browserSync.stream());
 }
 
 function htmlfileinclude() {
-	return gulp
-		.src("./src/html/*.html")
-		.pipe(
-			fileinclude({
-				prefix: "@@",
-				basepath: "@file",
-			})
-		)
-		.pipe(gulp.dest("./src/"))
-		.pipe(browserSync.stream());
+    return gulp
+        .src("./src/html/*.html")
+        .pipe(
+            fileinclude({
+                prefix: "@@",
+                basepath: "@file",
+            })
+        )
+        .pipe(gulp.dest("./dist/"))
+        .pipe(browserSync.stream());
+}
+
+function copyAssets() {
+    return gulp
+        .src(["./src/**/*", "!./src/**/*.html", "!./src/html", "!./src/partial"])
+        .pipe(gulp.dest("./dist/"));
 }
 
 function watch() {
-	browserSync.init({
-		server: {
-			baseDir: "./src/",
-		},
-	});
+    browserSync.init({
+        server: {
+            baseDir: "./dist/",
+        },
+    });
 
-	gulp.watch("./src/html/*.html", htmlfileinclude);
-	gulp.watch("./src/partial-html/*.html", htmlfileinclude);
-	gulp.watch("./src/assets/sass/**/*.scss", style);
+    gulp.watch("./src/html/*.html", series(htmlfileinclude, copyAssets));
+    gulp.watch("./src/partial-html/*.html", series(htmlfileinclude, copyAssets));
+    gulp.watch("./src/assets/sass/**/*.scss", style);
 
-	gulp.watch("./src/html/*.html").on("change", browserSync.reload);
-	gulp.watch("./src/partial-html/*.html").on("change", browserSync.reload);
-	gulp.watch("./src/assets/sass/**/*.scss").on("change", browserSync.reload);
+    gulp.watch("./dist/*.html").on("change", browserSync.reload);
 }
 
 exports.watch = watch;
+exports.build = series(htmlfileinclude, style, copyAssets);
