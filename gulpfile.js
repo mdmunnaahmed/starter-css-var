c0f7d28056291a88b9c1cc832a254c26195f0851
mdmunnaahmed <msdmunna77@gmail.com>
2024-07-13 14:22:24 +0600

    all variables --gulpjs --updated

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,22 @@ const { series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const fileinclude = require("gulp-file-include");
 const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
 function style() {
-  return gulp.src("./src/assets/sass/main.scss").pipe(sass().on("error", sass.logError)).pipe(gulp.dest("./src/assets/css")).pipe(browserSync.stream());
+  return gulp
+    .src("./src/assets/sass/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      postcss([
+        autoprefixer({
+          cascade: false,
+        }),
+      ])
+    )
+    .pipe(gulp.dest("./src/assets/css"))
+    .pipe(browserSync.stream());
 }
 
 function htmlfileinclude() {
@@ -31,7 +44,6 @@ function watch() {
   gulp.watch("./src/pages/*.html", htmlfileinclude);
   gulp.watch("./src/partial/*.html", htmlfileinclude);
   gulp.watch("./src/assets/sass/**/*.scss", style);
-
 }
 
 exports.watch = watch;
