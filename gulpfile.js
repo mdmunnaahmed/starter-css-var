175fc8c8cff963c70b972da0586dc550c6bea829
mdmunnaahmed <msdmunna77@gmail.com>
2024-07-13 12:50:15 +0600

    dist folder stystem --removed

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,39 @@
 const gulp = require("gulp");
 const { series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const concat = require("gulp-concat");
 const fileinclude = require("gulp-file-include");
 const browserSync = require("browser-sync").create();
 
 function style() {
-    return gulp
-        .src("./src/assets/sass/main.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("./src/assets/css"))
-        .pipe(gulp.dest("./dist/assets/css"))
-        .pipe(browserSync.stream());
+  return gulp.src("./src/assets/sass/main.scss").pipe(sass().on("error", sass.logError)).pipe(gulp.dest("./src/assets/css")).pipe(browserSync.stream());
 }
 
 function htmlfileinclude() {
-    return gulp
-        .src("./src/pages/*.html")
-        .pipe(
-            fileinclude({
-                prefix: "@@",
-                basepath: "@file",
-            })
-        )
-        .pipe(gulp.dest("./dist/"))
-        .pipe(browserSync.stream());
-}
-
-function copyAssets() {
-    return gulp
-        .src(["./src/**/*", "!./src/**/*.html", "!./src/pages", "!./src/partial", "!./src/assets/sass/**"])
-        .pipe(gulp.dest("./dist/"));
+  return gulp
+    .src("./src/pages/*.html")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(gulp.dest("./src/")) // Export HTML files directly to the src folder
+    .pipe(browserSync.stream());
 }
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: "./dist/",
-        },
-    });
+  browserSync.init({
+    server: {
+      baseDir: "./src/",
+    },
+  });
 
-    gulp.watch("./src/pages/*.html", series(htmlfileinclude, copyAssets));
-    gulp.watch("./src/partial/*.html", series(htmlfileinclude, copyAssets));
-    gulp.watch("./src/assets/sass/**/*.scss", style); // Include this line to watch Sass files
+  gulp.watch("./src/pages/*.html", htmlfileinclude);
+  gulp.watch("./src/partial/*.html", htmlfileinclude);
+  gulp.watch("./src/assets/sass/**/*.scss", style);
 
-    gulp.watch("./dist/*.html").on("change", browserSync.reload);
+  gulp.watch("./src/*.html").on("change", browserSync.reload);
 }
 
 exports.watch = watch;
-exports.build = series(htmlfileinclude, style, copyAssets);
+exports.build = series(htmlfileinclude, style);
